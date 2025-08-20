1a2a18757725992ff6f19e34a6919ce8ad1dc727
Josh Franey <josh.franey@outlook.com>
2025-08-20 15:00:35 -0400

    Add logging at startup

--- SensorPull/Program.cs
+++ SensorPull/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using SensorPull.Models.Configuration;
 using SensorPull.Services;
 
@@ -16,6 +17,18 @@ var host = new HostBuilder()
 #endif
     })
     .ConfigureFunctionsWorkerDefaults()
+    .ConfigureLogging(logging =>
+    {
+        // Ensure your categories are visible
+        logging.SetMinimumLevel(LogLevel.Information);
+        logging.AddFilter("Microsoft", LogLevel.Warning);
+        logging.AddFilter("Worker", LogLevel.Information);
+
+        // Replace these namespaces with YOUR real ones:
+        logging.AddFilter("SensorPull.Functions.ThermostatTimer", LogLevel.Information);
+        logging.AddFilter("SensorPull.Services.SensorPushClient", LogLevel.Information);
+        logging.AddFilter("SensorPull.Services.GoveeClient", LogLevel.Information);
+    })
     .ConfigureServices((context, services) =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
